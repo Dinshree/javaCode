10f3400b9be1d78fb755b6246424c415a2b6a13a
Dinshree <dinshree82@gmail.com>
2025-07-29 17:46:51 +0530

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,129 @@
+// // // // // //array
+// // // // // let fruits=["apple", "mango", "banana"];
+// // // // // console.log(fruits,fruits[2]);
+// // // // // //push method 
+// // // // // fruits.push("chiku")
+// // // // // console.log(fruits);
+// // // // // fruits.push("waterlemon")
+// // // // // console.log(fruits);
+// // // // // //unshift
+// // // // // fruits.unshift("grapes")
+
+
+// // // // // //pop 
+// // // // // fruits.pop();
+// // // // // console.log(fruits);
+
+// // // // // //shift
+// // // // // fruits.shift()
+// // // // // console.log(fruits);
+// // // // // //sort
+// // // // // let num=[1,5,2,6,8,3];
+// // // // // // num.sort()
+// // // // // // console.log(num);
+// // // // // //to sort
+// // // // // let a=num.toSorted()
+
+// // // // // console.log(a);
+// // // // // //reverse
+// // // // // fruits.reverse()
+// // // // // console.log(fruits);
+// // // // // //toreverse
+// // // // // let b= fruits.toReversed()
+// // // // // console.log(fruits);
+
+// // // // //*********************************************************************************************************************8
+// // // // //string
+
+// // // // let s ="my   name is dinshree";
+// // // // console.log(s.length);
+// // // // console.log(s.toUpperCase());
+// // // // console.log(s.toLowerCase());
+// // // // console.clear();
+// // // // console.log(s.length);
+// // // // console.log(s.startsWith("my"));
+// // // // console.log(s.endsWith("is"));
+// // // // console.log(s.substring(5,9));
+// // // // console.log(s.indexOf("e",4));
+// // // // console.log(s.split(" "));
+// // // // console.log(typeof(s.split(" ")));
+
+
+// // // //************************************************************************************************************* */
+// // // //objects
+
+// // // let student ={
+// // //     name:"dinshree",
+// // //     age:30,
+// // //     city:"pune",
+// // // }
+// // // let rd = Object.keys(student);
+// // // console.log(rd);
+// // // let bd = Object.keys(student);
+// // // console.log(bd);
+
+// // //*************************************************************** */
+// // //ctrl flow
+
+
+// // let a = 30;
+// //  if(a>40){
+// //     console.log("a is greater than 40");
+// //  }
+// //  else{
+// //      console.log("a issmaller than 40");
+// //  }
+
+
+// // let name ="";
+// // if (name.length ){
+// //    console.log("yes");
+// // }
+// // else {
+// //     console.log ('no');
+// // }
+
+
+
+// // for( let i=1; i<=10; i++){
+// //     console.log("a",i);
+// // }
+
+
+
+// //*************************************************************************************************
+
+// //JSON=   JAVASCRIPT OBJECT NOTATION
+// //KEY :VALUE PAIRS
+
+// //function
+
+
+// function js(){
+//     console.log("hello world");
+//     console.log("hello world");
+// }
+// js();
+
+
+// function add(num1,num2){
+//     return num1+num2;
+// }
+// console.log(add(2,4));
+
+//**********************************************************************************************888 */
+
+
+
+let subtract =(a,b) => a-b;
+
+
+console.log(subtract(100, 50));
+
+let add =(a,b) => a+b;
+ function add (a,b){
+    return (a+b);
+ }
+ 
+
+
