b5df2b47f311332d5988a1825578506057f49d95
Dinshree <dinshree82@gmail.com>
2025-07-29 17:33:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+var name = "dinshree";
+let name3;
+let name1 = "dinshree";
+const name2 = "dinshree";
+name="bhubharva";
+name1="bhubharva";
+console.log("my name is ", name);
+console.log("my name is ", name1);
+console.log("my name is ", 54+67);
+
+// function myName() {
+//     name3='gygh';
+//     console.log("Infunction", name, name1, name2, name3);
+// }
+// console.log(myName());
+console.log("my name3 is ", name3);
+let person ={
+    name:"dinshree",
+    age:20,
+    city:"pune",
+}
+person.age=25;
+console.log("object name" , person.name, person.age);
+
+
+let book ={
+     name:"abc",
+     author:"xyz",
+    }
+    book.author="rty";
+    console.log ( book.author);
+    let bh ={
+        name:"bhubhu",
+        city:"pune",
+}
+console.log( bh.city);
